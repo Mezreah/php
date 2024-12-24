b4aa41ba24eb5ad20191598d191d2e0d9e11a1b4
Yanis <156795279+Mezreah@users.noreply.github.com>
2024-12-24 22:18:24 +0100

    Add files via upload

--- Projet_php/index.php
+++ Projet_php/index.php
@@ -5,3 +5,21 @@ session_start();
 include "View/header.php";
 include "Controller/root.php";
 include "View/footer.php";
+
+
+require_once '../Controller/class.php';
+
+/*
+require_once 'Router.php';
+require_once 'controllers/HomeController.php';
+require_once 'models/HomeModel.php';
+*/
+
+$router = new Router();
+
+$router->addRoute('/', function() {
+    $controller = new controller();
+    $controller->index();
+});
+
+$router->handleRequest($_SERVER['REQUEST_URI']);
